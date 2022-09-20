ee8c732fd5a332d04c978e90c0b38e157279f25f
Vishnupriya9943 <101937629+Vishnupriya9943@users.noreply.github.com>
2022-09-20 11:14:29 +0530

    Add files via upload

---
+++ Assessment/Vishnu Priya/Assignment 1/sizzling_jarv_maimu1.ino
@@ -0,0 +1,59 @@
+// C++ code
+//
+// C++ code
+//
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(4, INPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  double data=analogRead(A3);
+  double n = data/1024;
+  double volt = n*5;
+  double off = volt-0.5;
+  double temperature = off * 100;
+  
+  if(temperature > 60){
+    Serial.print("Temperature is above 60C : ");
+    Serial.print(temperature);
+    Serial.print("C");
+    
+    
+    tone(13,1800,100);
+    delay(1000);
+    tone(13,1800,1000);
+    delay(100);
+    
+  }
+  else{
+    Serial.print("Temperature is above 60C : ");
+    Serial.print(temperature);
+    Serial.print("C");
+    Serial.print("\n\n");
+    
+    noTone(13);
+  }
+  
+  
+  //for PIR
+  int motion = digitalRead(2);
+  Serial.print("Position is: ");
+  Serial.print(motion);
+  
+  if(motion == 1){
+    Serial.println("Motion Detected");
+    
+    tone(13,1800,100);
+    delay(1000);
+    tone(13,1800,1000);
+    delay(100);
+  }
+  else{
+    Serial.println("No Motion");
+    noTone(13);
+  }
+}
